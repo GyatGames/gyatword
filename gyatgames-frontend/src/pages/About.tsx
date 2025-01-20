dc2d08f717396da1a948dee90d28bc8b96e93759
David Chan <davidchanwz@gmail.com>
2025-01-21 01:22:47 +0800

    fixed about

--- gyatgames-frontend/src/pages/About.tsx
+++ gyatgames-frontend/src/pages/About.tsx
@@ -21,36 +21,32 @@ export const About = () => {
               <TeamCard
                 name="David Chan"
                 role="Frontend Developer"
-                image="https://media.licdn.com/dms/image/v2/D5603AQFiFsS7PnCBgw/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1722863876474?e=1743033600&v=beta&t=jU-LXE-ou-4mH4nfri5HWNiSxflKxKbgEA3JwUAf2sA"
+                image="/david.jpg"
                 github="https://github.com/davidchanwz"
-                twitter="https://twitter.com/leo_mirand4"
                 linkedin="https://www.linkedin.com/in/davidchanwz/"
                 description="I love gyat."
               />
               <TeamCard
                 name="Aayush Sharma"
                 role="Frontend Developer"
-                image="https://media.licdn.com/dms/image/v2/D5603AQGcFfqNcCWF_g/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1680314103058?e=1743033600&v=beta&t=XYjlhBakeqmUBzhSEzBwUKxyDgSbAAe3aYlo7JwCnoU"
+                image="/aayush.jpg"
                 github="https://github.com/ahhyush"
-                twitter="https://twitter.com/janedoe"
                 linkedin="https://www.linkedin.com/in/aayush-sharma-329321208/"
                 description="I really love gyat."
               />
               <TeamCard
                 name="Jensen Huang"
                 role="Backend Developer"
-                image="https://media.licdn.com/dms/image/v2/D5603AQGtnQFGiiXcyg/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1698040588935?e=1743033600&v=beta&t=Tvr_Sb7Rw_dFAO20aD-fYXGQrMiNt12mewydpmSWTLU"
+                image="/jensen.jpg"
                 github="https://github.com/jensenhuangyankai"
-                twitter="https://twitter.com/johnsmith"
                 linkedin="https://www.linkedin.com/in/jensenhyk/"
                 description="I really really love gyat."
               />
               <TeamCard
                 name="Benjamin Koh"
                 role="Backend Developer"
-                image="https://media.licdn.com/dms/image/v2/D5603AQFrg5KJI0fDgw/profile-displayphoto-shrink_400_400/B56ZOl4J3oGoAg-/0/1733654784708?e=1743033600&v=beta&t=ngREuivWICXZ5P7fJvHiQenRu7SwCLNx6lfCuLv1Nzw"
+                image="/ben.jpg"
                 github="https://github.com/Ben926"
-                twitter="https://twitter.com/emilydavis"
                 linkedin="https://www.linkedin.com/in/benjaminkoh926/"
                 description="I really really really love gyat."
               />
